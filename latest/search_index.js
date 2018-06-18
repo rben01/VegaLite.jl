b2ae0afd56fc3aece7c9f935cf89b5527c18c69c
autodocs <autodocs>
2018-06-18 05:18:59 +0000

    build based on e44b8b1

--- latest/search_index.js
+++ latest/search_index.js
@@ -73,51 +73,99 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "gettingstarted/quick.html#",
-    "page": "Quick Tour",
-    "title": "Quick Tour",
+    "location": "gettingstarted/tutorial.html#",
+    "page": "Tutorial",
+    "title": "Tutorial",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "gettingstarted/quick.html#Quick-tour-1",
-    "page": "Quick Tour",
-    "title": "Quick tour",
+    "location": "gettingstarted/tutorial.html#Tutorial-1",
+    "page": "Tutorial",
+    "title": "Tutorial",
     "category": "section",
-    "text": "note: Note\nThis section is outdated and does not reflect the latest API of the package."
+    "text": "This tutorial will show you how to create plots with VegaLite.jl."
 },
 
 {
-    "location": "gettingstarted/quick.html#Simple-scatter-plot-1",
-    "page": "Quick Tour",
-    "title": "Simple scatter plot",
+    "location": "gettingstarted/tutorial.html#Data-1",
+    "page": "Tutorial",
+    "title": "Data",
     "category": "section",
-    "text": "Use functions linked by the |> operator to build your visuialization incrementally. VegaLite.jl can use DataFrames or DataTables as sources for data. For a scatter plot, specify that the mark should a point by markpoint(), then how the data in the DataFrame mpg (fields :Cty, :Hwy and :Manufacturer) should be connected to the encoding channels (x, y and color respectively). Finally, global configuration options are provided in a config function (type ? config`to see all the options).using RDatasets\n\nmpg = dataset(\"ggplot2\", \"mpg\") # load the \'mpg\' dataframe\n\nmpg |> # start with the data source (here a DataFrame)\n  markpoint() |>\n  encoding(xquantitative(field=:Cty, axis=nothing),\n           yquantitative(field=:Hwy, vlscale(zero=false)),\n           colornominal(field=:Manufacturer)) |>    # bind color to :Manufacturer, nominal scale\n  config(vlcell(width=350, height=400))(Image: plot1)An alternative, more julian syntax, without the piping operator is also valid. The preceding statement can just as well be written :plot(data(mpg),\n     markpoint(),\n     encoding(xquantitative(field=:Cty, axis=nothing),\n              yquantitative(field=:Hwy, vlscale(zero=false)),\n              colornominal(field=:Manufacturer)),\n     config(vlcell(width=350, height=400)))"
+    "text": "Plots are a way to visualize data, and therefore every plot starts with some dataset. VegaLite.jl expects data to be in tabular form, and it works best if you pass it tidy data.The definition of tabular data we are using here is very simple: think of a table that has a header and consists of a number of columns and rows. The header gives each column a name. The rows correspond to the actual data. Columns don\'t have to be of the same data type, i.e. you can have one column that contains Strings, and another that contains Float64 values.VegaLite.jl can digest many different julia types that store tabular data. You can, for example, plot data that is stored in a DataFrame, JuliaDB.jl or loaded from disc with CSVFiles.jl. In this tutorial we will plot data that ships in the package VegaDatasets.jl.The dataset we will use for this tutorial is the cars dataset from the VegaDatasets.jl. The dataset contains information about a couple hundred cars. We can load the dataset with the dataset function:using VegaDatasets\n\ndata = dataset(\"cars\")We are storing the dataset in the variable data, so that we can access it more easily in the following examples."
 },
 
 {
-    "location": "gettingstarted/quick.html#Stacking-multiple-plots-with-hconcat()-or-vconcat()-1",
-    "page": "Quick Tour",
-    "title": "Stacking multiple plots with hconcat() or vconcat()",
+    "location": "gettingstarted/tutorial.html#Simple-Scatter-Plot-1",
+    "page": "Tutorial",
+    "title": "Simple Scatter Plot",
     "category": "section",
-    "text": "Several independant plots can be concatenated vertically or horizontally :using RDatasets\n\nmpg = dataset(\"ggplot2\", \"mpg\") # load the \'mpg\' dataframe\n\nr1 = markline() |>\n     encoding(xquantitative(field=:Cty, axis=nothing),\n              yquantitative(field=:Hwy, vlscale(zero=false)),\n              colornominal(field=:Manufacturer)) ;\n\nr2 = markrect() |>\n      encoding(xquantitative(field=:Displ, vlbin(maxbins=5)),\n               yquantitative(field=:Hwy, vlbin(maxbins=5)),\n               colornominal(field=:Manufacturer)) ;\n\nmpg |>\n  vconcat(r1) |>\n  vconcat(r2) |>\n  config(vlcell(width=400))(Image: plot2)"
+    "text": "We will start out with a very simple scatter plot. The minimum steps for creating a plot are to 1) pass the data we want to plot to the plot macro, and 2) specify what kind of visual shape, or \"mark\" in Vega-Lite terminology, we want to use to visualize our data. We \"pipe\" the data into the plot macro using the pipe operator |>. The actual plot is created by a call to the @vlplot macro. The first argument to the @vlplot macro specifies what kind of mark we want to use for our plot. We pass the name of the mark as a symbol. In our example we want to use points for our plot. The following code shows this minimal plot:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |> @vlplot(:point)While this code produces a plot, it is not a very useful plot. Vega-Lite is actually drawing a point for every row in our input dataset. But it is drawing all these points on top of each other, which makes the plot so uninteresting.To create a more interesting plot, we next need to specify how Vega-Lite should connect key properties of the points (for example their position) with the data that we passed it. These connections are called \"encodings\" in Vega-Lite. We will start out by specifying how both the x and y position encoding channel for the points should take values from the data we passed:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |> @vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower)This code now produces a nice scatter plot. We specify these encodings by using keyword arguments inside the @vlplot macro that correspond to the names of the encoding channels, in our example the x and y channel. We pass the names of the columns in our dataset that we want to use for these channels as symbols, e.g. as :Miles_per_Gallon and :Horsepower."
 },
 
 {
-    "location": "gettingstarted/quick.html#Using-layer()-to-surimpose-several-plots-1",
-    "page": "Quick Tour",
-    "title": "Using layer() to surimpose several plots",
+    "location": "gettingstarted/tutorial.html#Encoding-1",
+    "page": "Tutorial",
+    "title": "Encoding",
     "category": "section",
-    "text": "When several marks need to shown on the same visualization, use the layer() function :using DataFrames\n\ndf  = DataFrame(x=[0:5;], y=rand(6))\n\nencx = xquantitative(field=:x)\nency = yquantitative(field=:y)\n\ndf |>\n  plot(width=500) |>\n  layer(markline(interpolate=\"linear\"),\n        encoding(encx, ency, vlcolor(value=\"green\"))) |>\n  layer(markline(interpolate=\"basis\"),\n        encoding(encx, ency, vlcolor(value=\"red\"))) |>\n  layer(markpoint(), encoding(encx, ency, vlcolor(value=\"black\")))(Image: plot4)"
+    "text": "Vega-Lite provides many different encoding channels beyond the x and y channel we saw in the previous section. This section will introduce a few more encoding channels and how you can configure their details. You can read about the full list of encoding channels in the original Vega-Lite documentation."
 },
 
 {
-    "location": "gettingstarted/quick.html#Using-repeat()-to-facet-a-plot-by-data-fields-1",
-    "page": "Quick Tour",
-    "title": "Using repeat() to facet a plot by data fields",
+    "location": "gettingstarted/tutorial.html#Channels-1",
+    "page": "Tutorial",
+    "title": "Channels",
     "category": "section",
-    "text": "To create a facet plot with encoding channels cycled through a list of data fields use the repeat() function :using Distributions, DataTables\n\nxs = rand(Normal(), 100, 3)\ndt = DataTable(a = xs[:,1] + xs[:,2] .^ 2,\n               b = xs[:,3] .* xs[:,2],\n               c = xs[:,3] .+ xs[:,2])\n\ndt |>\n  repeat(column = [:a, :b, :c], row = [:a, :b, :c]) |>\n  config(vlcell(width=100, height=100)) |>\n  spec(markpoint(),\n       encoding(xquantitative(vlfield(repeat=:column)),\n                yquantitative(vlfield(repeat=:row))))(Image: plot3)"
+    "text": "As our next step we will encode the color channel. The following code will use the Origin column in our dataset for the color channel, so that the points in our plot use a different color for each unique value in the Origin column:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |> @vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, color=:Origin)If we want to produce a separate plot for each of the three unique Origin values, we can instead encode the columns channel so that we create a facet plot:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |> @vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, column=:Origin)We can now use the color channel to visualize yet another column from our dataset. The following code uses the color encoding channel to visualize the Cylinders column in our dataset:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |>\n@vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, column=:Origin, color=:Cylinders)"
+},
+
+{
+    "location": "gettingstarted/tutorial.html#Channel-types-1",
+    "page": "Tutorial",
+    "title": "Channel types",
+    "category": "section",
+    "text": "Before we introduce channel types, we will go back to a more simple plot without faceting.using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |>\n@vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, color=\"Cylinders\")We are still encoding the color channel, but note that we are now passing the name of the column as a String, not as a Symbol (i.e. we are writing color=\"Cylinders\" instead of color=:Cylinders). This is a general pattern in VegaLite.jl: you can generally use Symbols and Strings interchangeably. Using a Symbol saves you one extra character (the closing \"), so we tend to use those when possible, but sometimes you need to use characters that can\'t be used in Julia\'s literal Symbolsyntax, and then we use Strings.The legend that was automatically generated for the color channel in the previous plot uses a continuous scale, i.e. it shows a smooth range of colors. VegaLite.jl automatically encodes any numeric column in the source data as such a \"quantitative\" channel. Sometimes that is not a good automatic default, though. For example, in our example case there are only a handful of distinct integer values used in the Cylinders column, and in such a case we might prefer a more discrete legend.We can tell VegaLite.jl to change the type of a channel from say the default quantitative type to an ordinal channel by slightly changing the channel encoding to color=\"Cylinders:ordinal\". In this case we specify the type of the encoding as a second argument in the string that is separated from the column name by a :. We can further shorten this by writing color=\"Cylinders:o\", i.e. we can only use the first character of the type of encoding we want to use.Using this new syntax we can generate the following plot:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |>\n@vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, color=\"Cylinders:o\")Values in an ordinal channel are still ordered, so Vega-Lite automatically picks a color scheme that can showcase such an order.What if we don\'t want to use a color scheme that signals any order? In that case we can change the type of the encoding to nominal by using the syntax color=\"Cylinders:n\", generating the following plot:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |>\n@vlplot(:point, x=:Miles_per_Gallon, y=:Horsepower, color=\"Cylinders:n\")You can also use the same encoding type specification for any other encoding channel. The following example puts the Cylinders column on the x axis of the plot and specifies it as an ordinal encoding:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |> @vlplot(:point, x=\"Cylinders:o\", y=:Miles_per_Gallon)"
+},
+
+{
+    "location": "gettingstarted/tutorial.html#Channel-properties-1",
+    "page": "Tutorial",
+    "title": "Channel properties",
+    "category": "section",
+    "text": "Our previous plots looked quite decent, but in many cases we probably still want to customize a whole range of features of our plots. One simple example in the previous plots might be the label of the axis that encoded the Miles_per_Gallon column. The axis automatically got labeled by the column name, and we might want to remove the underscores from the actual axis label.Whenever we want to specify more properties for a channel than just the name or type, we have to assign a composite value to the name of the channel by using curly brackets {}. Note that this use of {} is specific to the @vlplot macro, i.e. it is not a generic Julia language feature. For example, to specify an alternative title for an axis, we would write x={:Miles_per_Gallon, title=\"MPG\"}. Note that within the composite value we can still pass the name of the field to be encoded as a first positional argument, followed by arbitrary many named arguments. We can use similar syntax to also adjust the title of the legend, as shown in the following code example:using VegaLite, VegaDatasets\n\ndata = dataset(\"cars\")\n\ndata |>\n@vlplot(\n    :point,\n    x={\n        :Miles_per_Gallon,\n        title=\"MPG\"\n    },\n    y=:Horsepower,\n    color={\n        \"Cylinders:n\",\n        legend={title=\"No of Cylinders\"}\n    }\n)Channels have a large number of properties that you can customize in this way, they are all explained in the original Vega-Lite documentation."
+},
+
+{
+    "location": "gettingstarted/tutorial.html#Marks-1",
+    "page": "Tutorial",
+    "title": "Marks",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "gettingstarted/tutorial.html#Aggregations-1",
+    "page": "Tutorial",
+    "title": "Aggregations",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "gettingstarted/tutorial.html#Configurations-1",
+    "page": "Tutorial",
+    "title": "Configurations",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "gettingstarted/tutorial.html#File-IO-1",
+    "page": "Tutorial",
+    "title": "File IO",
+    "category": "section",
+    "text": "TODO"
 },
 
 {
